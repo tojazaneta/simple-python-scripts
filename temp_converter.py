04080492025dcbfce2e437ce9deeab5bc0740153
Żaneta S <zaneta.stanczak@gmail.com>
2018-05-19 18:47:01 +0200

    initial commit

--- temp_converter.py
+++ temp_converter.py
@@ -1,4 +1,13 @@
 """Temperature converter. Convert Celsius to Fahrenheit."""
 
-def c_t_f(celsius):
+celsius = input("Celsius: ")
 
+def c_t_f():
+    if float(celsius) < -273.15:
+        print("Really, too cold")
+    else:
+        fahrenheit = float(celsius) * 9/5 + 32
+        print(celsius, "degrees Celsius =", fahrenheit, "degrees Fahrenheit")
+        print("---------------")
+
+c_t_f()
